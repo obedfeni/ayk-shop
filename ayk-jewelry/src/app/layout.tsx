810977bcffbc576878f1ccb9f6926b56b6972e10
obedfeni <101922090+obedfeni@users.noreply.github.com>
2026-03-13 13:05:00 -0700

    Update layout.tsx

--- ayk-jewelry/src/app/layout.tsx
+++ ayk-jewelry/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { QueryProvider } from '@/lib/queryClient';
+import { QueryProvider } from '@/lib/query-provider';
 import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
